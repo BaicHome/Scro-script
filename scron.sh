892ed4edacc294857ce958f30c512249aebfa229
BaicHome <82888929+BaicHome@users.noreply.github.com>
2023-05-24 20:36:23 +0800

    Update scron.sh

--- scron.sh
+++ scron.sh
@@ -5,45 +5,84 @@ if ! command -v crontab &> /dev/null
 then
     echo "Crontab未安装，正在安装..."
     sudo apt-get install cron
-    yum install cron -y
 fi
 
-# 检测是否已安装zenity
-if ! command -v zenity &> /dev/null
-then
-    echo "zenity未安装，正在安装..."
-    sudo apt-get install zenity
-    yum install zenity -y
-fi
+# 要求用户选择/输入执行时间和需要执行的shell命令
+echo "请选择执行时间："
+echo "1. 每分钟"
+echo "2. 每小时"
+echo "3. 每天"
+echo "4. 每周"
+echo "5. 每月"
+read -p "请选择： " choice
 
-# 显示GUI图形界面，要求用户选择/输入执行时间和需要执行的shell命令
-time=$(zenity --list --title="选择执行时间" --text="请选择执行时间" --radiolist --column "选择" --column "时间" TRUE "每分钟" FALSE "每小时" FALSE "每天" FALSE "每周" FALSE "每月")
-if [ "$time" = "每分钟" ]
-then
-    time="* * * * *"
-elif [ "$time" = "每小时" ]
-then
-    time="0 * * * *"
-elif [ "$time" = "每天" ]
-then
-    hour=$(zenity --entry --title="输入小时数" --text="请输入小时数（0-23）")
-    minute=$(zenity --entry --title="输入分钟数" --text="请输入分钟数（0-59）")
-    time="$minute $hour * * *"
-elif [ "$time" = "每周" ]
-then
-    day=$(zenity --list --title="选择星期几" --text="请选择星期几" --radiolist --column "选择" --column "星期几" TRUE "星期日" FALSE "星期一" FALSE "星期二" FALSE "星期三" FALSE "星期四" FALSE "星期五" FALSE "星期六")
-    hour=$(zenity --entry --title="输入小时数" --text="请输入小时数（0-23）")
-    minute=$(zenity --entry --title="输入分钟数" --text="请输入分钟数（0-59）")
-    time="$minute $hour * * $day"
-elif [ "$time" = "每月" ]
-then
-    day=$(zenity --entry --title="输入日期" --text="请输入日期（1-31）")
-    hour=$(zenity --entry --title="输入小时数" --text="请输入小时数（0-23）")
-    minute=$(zenity --entry --title="输入分钟数" --text="请输入分钟数（0-59）")
-    time="$minute $hour $day * *"
-fi
-command=$(zenity --entry --title="输入命令" --text="请输入需要执行的shell命令")
+case $choice in
+    1)
+        time="* * * * *"
+        ;;
+    2)
+        time="0 * * * *"
+        ;;
+    3)
+        read -p "请输入小时数（0-23）：" hour
+        read -p "请输入分钟数（0-59）：" minute
+        time="$minute $hour * * *"
+        ;;
+    4)
+        echo "请选择星期几："
+        echo "1. 星期日"
+        echo "2. 星期一"
+        echo "3. 星期二"
+        echo "4. 星期三"
+        echo "5. 星期四"
+        echo "6. 星期五"
+        echo "7. 星期六"
+        read -p "请选择：" day_choice
+        case $day_choice in
+            1)
+                day="0"
+                ;;
+            2)
+                day="1"
+                ;;
+            3)
+                day="2"
+                ;;
+            4)
+                day="3"
+                ;;
+            5)
+                day="4"
+                ;;
+            6)
+                day="5"
+                ;;
+            7)
+                day="6"
+                ;;
+            *)
+                echo "无效的选择"
+                exit 1
+                ;;
+        esac
+        read -p "请输入小时数（0-23）：" hour
+        read -p "请输入分钟数（0-59）：" minute
+        time="$minute $hour * * $day"
+        ;;
+    5)
+        read -p "请输入日期（1-31）：" day
+        read -p "请输入小时数（0-23）：" hour
+        read -p "请输入分钟数（0-59）：" minute
+        time="$minute $hour $day * *"
+        ;;
+    *)
+        echo "无效的选择"
+        exit 1
+        ;;
+esac
+
+read -p "请输入需要执行的shell命令：" command
 
 # 将用户选择/输入的信息配置为计划任务定时执行
 (crontab -l ; echo "$time $command") | crontab -
-zenity --info --title="完成" --text="计划任务已添加"
+echo "计划任务已添加"
