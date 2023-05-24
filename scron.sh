2105a8ecd6c7e20960c62e2ecce48931c48097d6
BaicHome <82888929+BaicHome@users.noreply.github.com>
2023-05-24 21:02:48 +0800

    Update scron.sh

--- scron.sh
+++ scron.sh
@@ -1,88 +1,63 @@
 #!/bin/bash
 
-# 检测是否已安装crontab
+# 检测是否已安装 crontab，如未安装，则自动进行安装
 if ! command -v crontab &> /dev/null
 then
-    echo "Crontab未安装，正在安装..."
-    sudo apt-get install cron
+    echo "crontab 未安装，正在自动安装..."
+    if command -v apt-get &> /dev/null
+    then
+        sudo apt-get update
+        sudo apt-get install -y cron
+    elif command -v yum &> /dev/null
+    then
+        sudo yum update
+        sudo yum install -y cronie
+    else
+        echo "无法自动安装 crontab，请手动安装后再运行脚本。"
+        exit 1
+    fi
 fi
 
-# 要求用户选择/输入执行时间和需要执行的shell命令
+# 展示 GUI 图形化界面，让用户选择/输入执行时间和需要执行的命令
 echo "请选择执行时间："
 echo "1. 每分钟"
-echo "2. 每小时"
-echo "3. 每天"
-echo "4. 每周"
-echo "5. 每月"
-read -p "请选择： " choice
+echo "2. 每3分钟"
+echo "3. 每5分钟"
+echo "4. 每天0点30分"
+echo "5. 每周几"
+echo "6. 每月多少号"
+read -p "请输入选项（1-6）：" option
 
-case $choice in
+case $option in
     1)
-        time="* * * * *"
+        schedule="* * * * *"
         ;;
     2)
-        time="0 * * * *"
+        schedule="*/3 * * * *"
         ;;
     3)
-        read -p "请输入小时数（0-23）：" hour
-        read -p "请输入分钟数（0-59）：" minute
-        time="$minute $hour * * *"
+        schedule="*/5 * * * *"
         ;;
     4)
-        echo "请选择星期几："
-        echo "1. 星期日"
-        echo "2. 星期一"
-        echo "3. 星期二"
-        echo "4. 星期三"
-        echo "5. 星期四"
-        echo "6. 星期五"
-        echo "7. 星期六"
-        read -p "请选择：" day_choice
-        case $day_choice in
-            1)
-                day="0"
-                ;;
-            2)
-                day="1"
-                ;;
-            3)
-                day="2"
-                ;;
-            4)
-                day="3"
-                ;;
-            5)
-                day="4"
-                ;;
-            6)
-                day="5"
-                ;;
-            7)
-                day="6"
-                ;;
-            *)
-                echo "无效的选择"
-                exit 1
-                ;;
-        esac
-        read -p "请输入小时数（0-23）：" hour
-        read -p "请输入分钟数（0-59）：" minute
-        time="$minute $hour * * $day"
+        schedule="30 0 * * *"
         ;;
     5)
-        read -p "请输入日期（1-31）：" day
-        read -p "请输入小时数（0-23）：" hour
-        read -p "请输入分钟数（0-59）：" minute
-        time="$minute $hour $day * *"
+        read -p "请输入要执行的星期几（0-6，0表示周日）：" weekday
+        schedule="* * $weekday * *"
+        ;;
+    6)
+        read -p "请输入要执行的日期（1-31）：" day
+        schedule="* * $day * *"
         ;;
     *)
-        echo "无效的选择"
+        echo "无效选项，程序退出。"
         exit 1
         ;;
 esac
 
-read -p "请输入需要执行的shell命令：" command
+read -p "请输入要执行的 shell 命令：" command
 
 # 将用户选择/输入的信息配置为计划任务定时执行
-(crontab -l ; echo "$time $command") | crontab -
-echo "计划任务已添加"
+(crontab -l ; echo "$schedule $command") | crontab -
+
+echo "计划任务已添加。"
